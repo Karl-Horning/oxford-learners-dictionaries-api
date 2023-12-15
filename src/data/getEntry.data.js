8264351425c2c46b89785acdedd4c8c76edadcfd
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-12-15 15:05:19 +0000

    Broke up into more maintainable functions

--- src/data/getEntry.data.js
+++ src/data/getEntry.data.js
@@ -2,23 +2,37 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 const https = require("https");
 
+const BASE_URL = process.env.BASE_URL;
+const API_KEY = process.env.API_KEY;
+const FORMAT = "html";
+
+function buildApiUrl() {
+    const params = { format: FORMAT };
+    return `${BASE_URL}?${new URLSearchParams(params).toString()}`;
+}
+
+function buildRequestOptions() {
+    return {
+        method: "GET",
+        headers: {
+            accessKey: API_KEY,
+        },
+    };
+}
+
+function handleResponseData(data) {
+    try {
+        const response = JSON.parse(data);
+        return response.entryContent;
+    } catch (error) {
+        throw new Error("Error parsing JSON response");
+    }
+}
+
 function getEntry() {
     return new Promise((resolve, reject) => {
-        const baseUrl = process.env.BASE_URL;
-        const accessKey = process.env.API_KEY;
-
-        const params = {
-            format: "html",
-        };
-
-        const apiUrl = `${baseUrl}?${new URLSearchParams(params).toString()}`;
-
-        const options = {
-            method: "GET",
-            headers: {
-                accessKey: accessKey,
-            },
-        };
+        const apiUrl = buildApiUrl();
+        const options = buildRequestOptions();
 
         const req = https.request(apiUrl, options, (res) => {
             let data = "";
@@ -29,8 +43,8 @@ function getEntry() {
 
             res.on("end", () => {
                 try {
-                    const response = JSON.parse(data);
-                    resolve(response.entryContent);
+                    const entryContent = handleResponseData(data);
+                    resolve(entryContent);
                 } catch (error) {
                     reject(error);
                 }
