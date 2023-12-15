039b9314227341e9731fd6da8cdbfb515a4f2043
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-12-15 14:53:40 +0000

    Broke down into separate functions

--- src/services/formatEntry.service.js
+++ src/services/formatEntry.service.js
@@ -1,59 +1,74 @@
 const cheerio = require("cheerio");
 const getEntry = require("../data");
 
+const addStyleSheetToHead = (head, cssFile) => {
+    head.append(`<link rel="stylesheet" type="text/css" href="${cssFile}">`);
+};
+
+const addCharsetToHead = (head) => {
+    head.append('<meta charset="UTF-8" />');
+};
+
+const addCopyrightFooter = (main, q) => {
+    main.append(
+        `<footer><span class="copyright">(Definition of ${q} from Oxford Advanced Learner's Dictionary © Oxford University Press)</span></footer>`
+    );
+};
+
+const removeElements = ($, selectors) => {
+    selectors.forEach((selector) => $(selector).remove());
+};
+
 const formatEntry = (html) => {
     const $ = cheerio.load(html);
     const q = $("span.seo").text();
 
-    // Replace the header tag with a main tag. This also adds an opening main tag as there is only a header closing tag in the data from the API.
-    $("header").replaceWith(function () {
-        return $("<main>").html($(this).html());
-    });
+    // Replace the header tag with a main tag
+    $("header").replaceWith(() => $("<main>").html($("header").html()));
 
-    // Remove the autocomplete fields
-    $("span.autocompletes").remove();
-    // Removed '[word] [type]' (for example, 'test noun' from next to .seo)
-    $("span.browse").remove();
-    // Remove all of the collapsible elements (like 'Collocations')
-    $("span.collapse").remove();
-    // Remove the dictionary links
-    $("span.dictlink-g").remove();
-    // Removes the index of definitions that are available in the page
-    $("span.indexing").remove();
-    // Remove the idioms
-    $("span.idm-gs").remove();
-    // Remove the link to the idioms at the end of the page
-    $("span.jumplinks").remove();
-    // Remove the word list from the end of the page
-    $("span.mywordlist-g").remove();
-    // Remove the pronunciations at the end of the page
-    $("span.pracpron").remove();
-    // Remove the commas in 'see also'
-    $("span.sep").remove();
-    // Removes the symbols (like 'A1', 'OPAL W', 'OPAL S') from the page
-    $("span.symbol-g").remove();
-    // Removes the topics under 'see also'
-    $("span.topic-g").remove();
+    // Remove unnecessary elements
+    const selectorsToRemove = [
+        // Remove the autocomplete fields
+        "span.autocompletes",
+        // Removed '[word] [type]' (for example, 'test noun' from next to .seo)
+        "span.browse",
+        // Remove all of the collapsible elements (like 'Collocations')
+        "span.collapse",
+        // Remove the dictionary links
+        "span.dictlink-g",
+        // Removes the index of definitions that are available in the page
+        "span.indexing",
+        // Remove the idioms
+        "span.idm-gs",
+        // Remove the link to the idioms at the end of the page
+        "span.jumplinks",
+        // Remove the word list from the end of the page
+        "span.mywordlist-g",
+        // Remove the pronunciations at the end of the page
+        "span.pracpron",
+        // Remove the commas in 'see also'
+        "span.sep",
+        // Removes the symbols (like 'A1', 'OPAL W', 'OPAL S') from the page
+        "span.symbol-g",
+        // Removes the topics under 'see also'
+        "span.topic-g",
+        // Add more selectors as needed
+    ];
+    removeElements($, selectorsToRemove);
 
-    // Add a CSS file to the head
-    const cssFile = "style.css";
-    $("head").append(
-        `<link rel="stylesheet" type="text/css" href="${cssFile}">`
-    );
-    // Add the encoding to the head
-    $("head").append('<meta charset="UTF-8" />');
-    // Add back the copyright info
-    $("main").append(
-        `<footer><span class="copyright">(Definition of ${q} from Oxford Advanced Learner's Dictionary © Oxford University Press)</span></footer>`
-    );
+    // Manipulate head
+    const head = $("head");
+    addStyleSheetToHead(head, "style.css");
+    addCharsetToHead(head);
 
-    // Get the modified HTML
-    const modifiedHtml = $.html();
+    // Manipulate main
+    const main = $("main");
+    addCopyrightFooter(main, q);
 
-    return modifiedHtml;
+    return $.html();
 };
 
-getData = async () => {
+const getData = async () => {
     try {
         const html = await getEntry();
         const formattedEntry = formatEntry(html);
