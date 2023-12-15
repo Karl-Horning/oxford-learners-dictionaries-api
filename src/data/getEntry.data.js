58c809c2adb4197b25c96c191eae008762d854cf
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-12-15 15:06:42 +0000

    Added JSDoc and comments

--- src/data/getEntry.data.js
+++ src/data/getEntry.data.js
@@ -2,15 +2,24 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 const https = require("https");
 
+// Constants for configuration parameters
 const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.API_KEY;
 const FORMAT = "html";
 
+/**
+ * Builds the API URL with the specified format.
+ * @returns {string} The constructed API URL.
+ */
 function buildApiUrl() {
     const params = { format: FORMAT };
     return `${BASE_URL}?${new URLSearchParams(params).toString()}`;
 }
 
+/**
+ * Builds the request options with the required headers.
+ * @returns {Object} The options object for the HTTPS request.
+ */
 function buildRequestOptions() {
     return {
         method: "GET",
@@ -20,6 +29,12 @@ function buildRequestOptions() {
     };
 }
 
+/**
+ * Parses the response data and retrieves the entry content.
+ * @param {string} data - The raw response data.
+ * @returns {string} The entry content from the parsed JSON response.
+ * @throws {Error} Throws an error if there's an issue parsing the JSON response.
+ */
 function handleResponseData(data) {
     try {
         const response = JSON.parse(data);
@@ -29,6 +44,10 @@ function handleResponseData(data) {
     }
 }
 
+/**
+ * Makes an HTTP request to retrieve entry data.
+ * @returns {Promise<string>} A Promise that resolves with the formatted entry content.
+ */
 function getEntry() {
     return new Promise((resolve, reject) => {
         const apiUrl = buildApiUrl();
